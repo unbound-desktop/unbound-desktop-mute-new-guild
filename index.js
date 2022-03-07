6b68233cb78ac3652745d2c7b1c9d107d4c6cc8d
eternal <eternal.git@gmail.com>
2022-03-07 02:42:30 +0000

    Release

---
+++ index.js
@@ -0,0 +1,46 @@
+import Plugin from '@structures/plugin';
+
+import { Dispatcher } from '@webpack/common';
+import { filters, bulk } from '@webpack';
+import { bindAll } from '@utilities';
+
+const [
+   Notifications,
+   Guilds,
+   Settings
+] = bulk(
+   filters.byProps('updateGuildNotificationSettings'),
+   filters.byProps('getSanitizedRestrictedGuilds'),
+   filters.byProps('updateRemoteSettings')
+);
+
+export default class MuteNewGuild extends Plugin {
+   constructor(...args) {
+      super(...args);
+
+      bindAll(this, ['handleInvite']);
+   }
+
+   start() {
+      Dispatcher.subscribe('INVITE_ACCEPT_SUCCESS', this.handleInvite);
+   }
+
+   stop() {
+      Dispatcher.unsubscribe('INVITE_ACCEPT_SUCCESS', this.handleInvite);
+   }
+
+   handleInvite({ invite: { guild: { id } } }) {
+      Notifications.updateGuildNotificationSettings(id, {
+         muted: true,
+         suppress_everyone: false,
+         suppress_roles: false,
+         mobile_push: true
+      });
+
+      const guilds = new Set(Guilds.getSanitizedRestrictedGuilds());
+
+      guilds.delete(id);
+
+      Settings.updateRemoteSettings({ restrictedGuilds: Array.from(guilds) });
+   }
+}
